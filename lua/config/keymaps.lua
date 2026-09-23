77b7dd35ca4486531d334afb9d885bd79e2f12c1
Wariss Akin-Ola Alao Balogoun <wbalogou@f4r10s12.paris.42.school>
2026-09-23 22:15:13 +0200

    keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,33 @@ vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>",
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",
 	{ desc = "Rechercher dans les fichiers" })
 
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>",
+	{desc = "Afficher les buffers" })
+
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>",
+	{ desc = "Fichiers récents" })
+
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>",
+	{ desc = "Symboles du fichier" })
+
+vim.keymap.set("n", "<leader>fS", "<cmd>Telescope lsp_workspace_symbols<CR>",
+	{ desc = "Symboles du projet" })
+
+vim.keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<CR>",
+	{ desc = "Diagnostics LSP" })
+
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<CR>",
+	{ desc = "Git status" })
+
+vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<CR>",
+	{ desc = "Git commits" })
+
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<CR>",
+	{ desc = "Git branches" })
+
+vim.keymap.set("n", "<leader>gB", "<cmd>Telescope git_bcommits<CR>",
+	{ desc = "Commits du fichier" })
+
 -- 42 Norm
 vim.keymap.set("n", "<F1>", function()
 	require("42norm.header").stdheader()
